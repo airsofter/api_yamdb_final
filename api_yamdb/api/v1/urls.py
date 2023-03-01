63cb3b154c6a68575f971b4c5c2e04e66024e7cd
Андрей Николаевич <andreykaspb@gmail.com>
2023-03-01 23:21:30 +0300

    modified:   README.md 	modified:   api_yamdb/api/v1/urls.py

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import CommentViewSet, GroupViewSet, PostViewSet, FollowViewSet
+from .views import TitleViewSet, GenreViewSet, CategoryViewSet, ReviewViewSet, CommentViewSet,
 
 router = routers.DefaultRouter()
 
 router.register(r"titles", TitleViewSet, basename="titles")
 router.register(r"genres", GenreViewSet, basename="genres")
 router.register(r"categories", CategoryViewSet, basename="categories")
-router.register(r'follow', FollowViewSet, basename='followers')
+router.register(r'comments', CommentViewSet, basename='followers')
 
 urlpatterns = [
     path('', include(router.urls)),
