6c35d23dae13305e5bd38084f6516faeec79f2ca
Денис Набатов <nabatoffdenis@yandex.ru>
2023-03-02 20:34:13 +0300

    написал модели произведения, жанров и категорий

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,79 +1,76 @@
-import base64
+# import base64
 
-from django.core.files.base import ContentFile
-from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
-from rest_framework.validators import UniqueTogetherValidator
-
-from posts.models import Comment, Group, Post, Follow, User
+# from django.core.files.base import ContentFile
+# from rest_framework import serializers
+# from rest_framework.relations import SlugRelatedField
+# from rest_framework.validators import UniqueTogetherValidator
 
 
-class Base64ImageField(serializers.ImageField):
-    """Модуль с функциями декодирования base64 изображения"""
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='imgs.' + ext)
+# class Base64ImageField(serializers.ImageField):
+#     """Модуль с функциями декодирования base64 изображения"""
+#     def to_internal_value(self, data):
+#         if isinstance(data, str) and data.startswith('data:image'):
+#             format, imgstr = data.split(';base64,')
+#             ext = format.split('/')[-1]
+#             data = ContentFile(base64.b64decode(imgstr), name='imgs.' + ext)
 
-        return super().to_internal_value(data)
-
-
-class GroupSerializer(serializers.ModelSerializer):
-    """Сериализатор модели Group"""
-
-    class Meta:
-        model = Group
-        fields = ('id', 'title', 'slug', 'description')
+#         return super().to_internal_value(data)
 
 
-class PostSerializer(serializers.ModelSerializer):
-    """Сериализатор модели Post"""
-    author = SlugRelatedField(slug_field='username', read_only=True)
-    image = Base64ImageField(required=False, allow_null=True)
+# class GroupSerializer(serializers.ModelSerializer):
+#     """Сериализатор модели Group"""
 
-    class Meta:
-        fields = ('id', 'author', 'text', 'pub_date', 'image', 'group')
-        model = Post
+#     class Meta:
+#         pass
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    """Сериализатор модели Comment"""
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+# class PostSerializer(serializers.ModelSerializer):
+#     """Сериализатор модели Post"""
+#     author = SlugRelatedField(slug_field='username', read_only=True)
+#     image = Base64ImageField(required=False, allow_null=True)
 
-    class Meta:
-        fields = ('id', 'author', 'text', 'created', 'post')
-        model = Comment
-        read_only_fields = ('post',)
+#     class Meta:
+#         fields = ('id', 'author', 'text', 'pub_date', 'image', 'group')
+#         # model = Post
 
 
-class FollowSerializer(serializers.ModelSerializer):
-    """Сериализатор модели Follow"""
-    user = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username',
-        default=serializers.CurrentUserDefault()
-    )
-    following = serializers.SlugRelatedField(
-        slug_field='username',
-        queryset=User.objects.all(),
+# class CommentSerializer(serializers.ModelSerializer):
+#     """Сериализатор модели Comment"""
+#     author = serializers.SlugRelatedField(
+#         read_only=True, slug_field='username'
+#     )
 
-    )
+#     class Meta:
+#         fields = ('id', 'author', 'text', 'created', 'post')
+#         # model = Comment
+#         read_only_fields = ('post',)
 
-    class Meta:
-        model = Follow
-        fields = ('user', 'following')
-        validators = [
-            UniqueTogetherValidator(
-                queryset=Follow.objects.all(),
-                fields=['user', 'following']
-            )
-        ]
 
-    def validate(self, data):
-        if self.context['request'].user == data['following']:
-            raise serializers.ValidationError(
-                'Нельзя подписаться на самого себя!')
-        return data
+# class FollowSerializer(serializers.ModelSerializer):
+#     """Сериализатор модели Follow"""
+#     # user = serializers.SlugRelatedField(
+#     #     read_only=True,
+#     #     slug_field='username',
+#     #     default=serializers.CurrentUserDefault()
+#     # )
+#     # following = serializers.SlugRelatedField(
+#     #     slug_field='username',
+#     #     queryset=User.objects.all(),
+
+#     # )
+
+#     # class Meta:
+#     #     model = Follow
+#     #     fields = ('user', 'following')
+#     #     validators = [
+#     #         UniqueTogetherValidator(
+#     #             queryset=Follow.objects.all(),
+#     #             fields=['user', 'following']
+#     #         )
+#     #     ]
+
+#     def validate(self, data):
+#         if self.context['request'].user == data['following']:
+#             raise serializers.ValidationError(
+#                 'Нельзя подписаться на самого себя!')
+#         return data
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+# from users.models import User  Заглушка для нашей модели юзера
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название жанра'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название'
+    )
+    year = models.IntegerField(
+        verbose_name='Год издания'
+    )
+    category = models.ForeignKey(
+        Category,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Категория'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle',
+        blank=True,
+        related_name='titles',
+        verbose_name='Жанры'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.title} относится к жанру {self.genre}'
