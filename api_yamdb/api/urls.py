32a3088e798b31eb991cb4d25994298378735bbb
Денис Набатов <nabatoffdenis@yandex.ru>
2023-03-04 00:49:39 +0300

    Проверьте урлы, закомментил то, что было в апи

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -10,7 +10,8 @@ from api import views
 app_name = 'api'
 
 urlpatterns = [
-    path('v1/auth/signup/', SignupView.as_view(), name='signup'),
-    path('v1/auth/token/', TokenObtainView.as_view(), name='token_obtain'),
+    path('v1/', include('api.v1.urls')),
+    # path('v1/auth/signup/', SignupView.as_view(), name='signup'),
+    # path('v1/auth/token/', TokenObtainView.as_view(), name='token_obtain'),
     # path('v1/users/', UsersView.as_view(), name='users'),
 ]
--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,10 +1,15 @@
 # import base64
+import random
 
 # from django.core.files.base import ContentFile
 from rest_framework import serializers
-from reviews.models import Category, Comment, Genre, Review, Title
 # from rest_framework.relations import SlugRelatedField
 # from rest_framework.validators import UniqueTogetherValidator
+
+from reviews.models import Category, Comment, Genre, Review, Title
+
+from core.send_mail import send_mail
+from users.models import User
 
 
 # class Base64ImageField(serializers.ImageField):
@@ -58,3 +63,35 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
 
 
+class SignupSerializer(serializers.ModelSerializer):
+    """Сериализатор для регистрации пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def create(self, validated_data):
+        """Метод для создания пользователя."""
+
+        confirmation_code = str(
+            random.randint(1000000, 9999999)
+        )
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=confirmation_code,
+        )
+        user.save()
+        send_mail(
+            subject='YamDB support!',
+            message=f'Код подтверждения: {confirmation_code}',
+            from_email='yam.db@support.com',
+            to_email=validated_data['email']
+        )
+        return user
+
+
+class TokenObtainSerializer(serializers.Serializer):
+    """Сериализатор для получения токена."""
+    username = serializers.CharField()
+    confirmation_code = serializers.CharField()
