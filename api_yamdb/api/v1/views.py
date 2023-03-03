12ed0eef781060b15ea25798849e25fdae810c4b
Денис Набатов <nabatoffdenis@yandex.ru>
2023-03-03 22:34:45 +0300

    начало создания вьюх для жанров, категорий и произведений

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -1,51 +1,43 @@
-# from rest_framework import permissions, viewsets
-# from rest_framework.generics import get_object_or_404
-# from rest_framework.filters import SearchFilter
-# from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import permissions, viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
-# from .permissions import AuthorOrReadOnly
-# from .serializers import CommentSerializer, GroupSerializer, PostSerializer
-# from .serializers import FollowSerializer
+from .permissions import AuthorOrReadOnly
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
 
-# # from posts.models import Group, Post
-
-
-# class GroupViewSet(viewsets.ReadOnlyModelViewSet):
-#     """Вьюсет Group"""
-#     # queryset = Group.objects.all()
-#     serializer_class = GroupSerializer
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+from reviews.models import Genre, Category, Title, Review, Comment
 
 
-# class PostViewSet(viewsets.ModelViewSet):
-#     """Вьюсет Post"""
-#     # queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#     permission_classes = (
-#         permissions.IsAuthenticatedOrReadOnly, AuthorOrReadOnly,)
-#     pagination_class = LimitOffsetPagination
-
-#     def perform_create(self, serializer):
-#         serializer.save(author=self.request.user)
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет жанров"""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
 
-# class CommentViewSet(viewsets.ModelViewSet):
-#     """Вьюсет Comment"""
-#     serializer_class = CommentSerializer
-#     permission_classes = (
-#         permissions.IsAuthenticatedOrReadOnly,
-#         AuthorOrReadOnly,
-#     )
+class CategoryViewSet(viewsets.ModelViewSet):
+    """Вьюсет категорий"""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-#     def get_post(self):
-#         post_id = self.kwargs['post_id']
-#         return get_object_or_404(Post, pk=post_id)
 
-#     def get_queryset(self):
-#         return self.get_post().comments.all()
+class TitleViewSet(viewsets.ModelViewSet):
+    """Вьюсет произведений"""
+    serializer_class = TitleSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        AuthorOrReadOnly,
+    )
 
-#     def perform_create(self, serializer):
-#         serializer.save(author=self.request.user, post=self.get_post())
+    def get_post(self):
+        post_id = self.kwargs['post_id']
+        return get_object_or_404(Post, pk=post_id)
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=self.get_post())
 
 
 # class FollowViewSet(viewsets.ModelViewSet):
