08d8f2040ca82a9a01eab5a531d540b755245ed0
Андрей Николаевич <andreykaspb@gmail.com>
2023-03-06 14:06:02 +0300

    Changes to be committed: 	new file:   api_yamdb/users/migrations/0008_auto_20230305_0114.py 	modified:   api_yamdb/users/models.py

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,30 +1,36 @@
+"""Модель User и менеджер модели User."""
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin,
 )
 from django.db import models
 from django.core.validators import RegexValidator
 
+ROLE_CHOICE = (
+    ('admin', 'admin'),
+    ('user', 'user'),
+    ('moderator', 'moderator'),
+)
+
 
 class UserManager(BaseUserManager):
-    """Менеджер для модели User"""
+    """Менеджер модели User"""
 
     def create_user(
             self,
             email: str,
             username: str,
             password: str,
-            role='',
-            bio='',
-            first_name='',
-            last_name=''
+            role: str = 'user',
+            bio: str = None,
+            first_name: str = None,
+            last_name: str = None,
     ):
         if not email:
             raise ValueError('Пожалуйста введите email.')
+  
         if not username:
             raise ValueError('Пожалуйста введите имя пользователя.')
-
         email = self.normalize_email(email)
         user = self.model(
             email=email,
@@ -37,7 +43,6 @@ class UserManager(BaseUserManager):
         )
         user.save(using=self._db)
         return user
-
     def create_superuser(
             self,
             email,
@@ -64,9 +69,15 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    """Модель User."""
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    password = models.CharField(max_length=7)
+    bio = models.TextField(max_length=256, blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
     username = models.CharField(
         max_length=150,
         unique=True,
@@ -74,21 +85,19 @@ class User(AbstractBaseUser, PermissionsMixin):
             RegexValidator(
                 r'^[-a-zA-Z0-9_]+$',
                 message='Поле не соответсвует требованиям.',
-                code='invalid_username'
+                code='invalid_username',
             )
         ]
     )
-    password = models.CharField(max_length=7)
-    role = models.CharField(max_length=30, blank=True)
-    bio = models.TextField(max_length=256, blank=True)
-    is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(
+        max_length=30,
+        choices=ROLE_CHOICE,
+        default='user',
+    )
 
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email', 'password', 'role']
-
     def __str__(self):
         return self.username
