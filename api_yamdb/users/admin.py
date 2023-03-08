0bc33f3a0e136464c47b2bceef7f950f0d9d1e40
Андрей Николаевич <andreykaspb@gmail.com>
2023-03-08 13:40:54 +0300

    modified:   api_yamdb/reviews/admin.py 	modified:   api_yamdb/users/admin.py

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
-from users.models import User
+from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "role",
+    )
+    list_display_links = ("pk", "username", "email")
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_editable = ("role", "first_name", "last_name")
+    list_filter = ("role",)
+
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(User, UserAdmin)
