ee2121b86ac017f73e26a85230a5745d2606caaa
Андрей <116579108+spb-andrey-ka@users.noreply.github.com>
2023-03-08 22:54:31 +0300

    Revert "full auth"

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-
 from .views import (SignupView, TokenObtainView, UsersViewSet,
                     TitleViewSet, GenreViewSet, CategoryViewSet,
                     CommentViewSet, ReviewViewSet)
