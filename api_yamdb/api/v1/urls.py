34f121e120267ab7ac8fad27a7a1d34fb1e8b13f
Андрей Николаевич <andreykaspb@gmail.com>
2023-03-08 13:58:18 +0300

    modified:   api_yamdb/api/v1/urls.py 	modified:   api_yamdb/api_yamdb/urls.py

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 
 from .views import (SignupView, TokenObtainView, UsersViewSet,
                     TitleViewSet, GenreViewSet, CategoryViewSet,
                     CommentViewSet, ReviewViewSet)
 
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 
 router.register(r"titles", TitleViewSet, basename="titles")
 router.register(r"genres", GenreViewSet, basename="genres")
