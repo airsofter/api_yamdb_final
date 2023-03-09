9b5aa063db950cef2149258097f741bac7c89024
Андрей Николаевич <andreykaspb@gmail.com>
2023-03-09 11:44:44 +0300

    modified:   api_yamdb/api/v1/serializers.py 	modified:   api_yamdb/api/v1/views.py 	modified:   api_yamdb/api_yamdb/settings.py 	modified:   api_yamdb/reviews/models.py 	deleted:    api_yamdb/reviews/tests.py 	deleted:    api_yamdb/reviews/views.py

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -19,6 +19,7 @@ class SignupSerializer(serializers.Serializer):
     """Сериализатор для регистрации пользователя."""
     username = serializers.CharField(
         max_length=150,
+        required=True,
         validators=[
             RegexValidator(
                 r'^[-a-zA-Z0-9_]+$',
@@ -28,23 +29,29 @@ class SignupSerializer(serializers.Serializer):
         ],
     )
     email = serializers.EmailField(max_length=254, required=True)
+    
 
     def validate(self, data):
+        errors = {}
         username = data.get('username')
         email = data.get('email')
 
         if not email:
-            raise serializers.ValidationError('Введите правильный адрес электронной почты.')
+            errors['email'] = 'Введите правильный адрес электронной почты.'
         if not username:
-            raise serializers.ValidationError('Это поле не должно быть пустым.')
+            errors['username'] = 'Это поле не должно быть пустым.'
         if username == 'me':
-            raise serializers.ValidationError('Никнейм "me" запрещен.')
-        return data
+            errors['username'] = 'Никнейм "me" запрещен.'
 
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return data
     def create(self, validated_data):
         username = validated_data.get('username')
         email = validated_data.get('email')
         confirmation_code = random.randint(1000000, 9999999)
+
         user = User.objects.filter(Q(username=username) | Q(email=email)).first()
         print(user)
 
@@ -87,47 +94,14 @@ class TokenObtainSerializer(serializers.Serializer):
         user.save()
         return user
 
-
     def validate(self, data):
         username = data.get('username')
         confirmation_code = data.get('confirmation_code')
 
         user = get_object_or_404(User, username=username)
         if confirmation_code != user.confirmation_code:
-
             raise serializers.ValidationError('Неверный код подтверждения.')
         return data
-
-    class Meta:
-        model = User
-        fields = (
-            'username',
-            'email',
-            'first_name',
-            'last_name',
-            'bio',
-            'role',
-        )
-
-    def validate_email(self, value):
-        """Метод для валидации email по количеству символов."""
-        if len(value) > 254:
-            raise serializers.ValidationError('Длина email не должна превышать 254 символа.')
-        return value
-
-    def create(self, validated_data):
-        """Метод для создания пользователя."""
-
-        if validated_data.get('role') == ('admin' or 'moderator'):
-            user = User.objects.create_user(
-                **validated_data,
-                is_staff=True,
-            )
-            return user
-        user = User.objects.create_user(
-            **validated_data,
-        )
-        return user
 
 
 class UsersSerializer(serializers.ModelSerializer):
@@ -154,21 +128,23 @@ class UsersSerializer(serializers.ModelSerializer):
             'role',
         )
 
-
-    # def validate(self, data):
-    #     """Метод для валидации email по количеству символов."""
-    #     first_name = data.get('first_name')
-    #     last_name = data.get('last_name')
-    #     email = data.get('email')
-    #     if not (email or first_name or last_name):
-    #         raise serializers.ValidationError('Это поле не может быть пустым.')
-    #     if len(email) > 254:
-    #         raise serializers.ValidationError('Длина email не должна превышать 254 символа.')
-    #     if len(last_name) > 150:
-    #         raise serializers.ValidationError('Длина фамилии не должна превышать 150 символа.')
-    #     if len(first_name) > 150:
-    #         raise serializers.ValidationError('Длина имени не должна превышать 150 символа.')
-    #     return data
+    def validate(self, data):
+        errors = {}
+        username = data.get('username')
+        email = data.get('email')
+        user = User.objects.filter(Q(username=username) | Q(email=email)).first()
+        if not self.partial:
+            if not email:
+                errors['email'] = 'Введите правильный адрес электронной почты.'
+            if not username:
+                errors['username'] = 'Это поле не должно быть пустым.'
+        if username == 'me':
+            errors['username'] = 'Никнейм "me" запрещен.'
+        if user:
+            errors['user'] = 'Пользователь с таким username или email уже существует'
+        if errors:
+            raise serializers.ValidationError(errors)
+        return data
 
 
     def create(self, validated_data):
@@ -260,7 +236,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = ('id', 'title', 'text', 'author', 'score', 'pub_date')
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
         model = Review
 
     def validate(self, data):
