6665d96d5d83f515d44d182993b38daa5d959d10
Денис Набатов <nabatoffdenis@yandex.ru>
2023-03-13 11:32:28 +0300

    создал файл для фильтров

---
+++ api_yamdb/api/v1/filters.py
@@ -0,0 +1,20 @@
+from django_filters.rest_framework import CharFilter, FilterSet, NumberFilter
+from reviews.models import Title
+
+
+class TitleFilter(FilterSet):
+    name = CharFilter(
+        field_name='name', lookup_expr='contains'
+    )
+    category = CharFilter(
+        field_name='category__slug', lookup_expr='contains'
+    )
+    genre = CharFilter(
+        field_name='genre__slug', lookup_expr='contains'
+    )
+    year = NumberFilter(
+        field_name='year', lookup_expr='contains')
+
+    class Meta:
+        model = Title
+        fields = ['name', 'category', 'genre', 'year']
